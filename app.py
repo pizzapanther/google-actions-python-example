21a7a04def4b50c93e27c19cf0f8a174ae50df80
Paul Bailey <paul.m.bailey@gmail.com>
2017-11-02 20:10:04 +0000

    conversation init

--- app.py
+++ app.py
@@ -1,13 +1,63 @@
+#!/usr/bin/env python
+
 import os
+import json
 
 import tornado.ioloop
 import tornado.log
 import tornado.web
 
 class WeatherHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.write("Hello, world")
-
+  def start_conversation (self):
+    response = {
+      'expectUserResponse': True,
+      'expectedInputs': [
+        {
+          'possibleIntents': {'intent': 'actions.intent.TEXT'},
+          'inputPrompt': {
+            'richInitialPrompt': {
+              'items': [
+                {
+                  'simpleResponse': {
+                    'ssml': '<speak>What city would you like the weather for?</speak>'
+                  }
+                }
+              ]
+            }
+          }
+        }
+      ]
+    }
+    
+    self.set_header('Google-Assistant-API-Version', 'v2')
+    self.write(response)
+    
+  def get_weather (self, city):
+    response = {}
+    
+    self.set_header('Google-Assistant-API-Version', 'v2')
+    self.write(response)
+    
+  def get (self):
+    city = self.get_query_argument('city', '')
+    if city:
+      self.get_weather(city)
+      
+    else:
+      self.start_conversation()
+      
+  def post (self):
+    data = json.loads(self.request.body.decode('utf-8'))
+    print(data['conversation']['type'])
+    print(data['conversation']['conversationId'])
+    
+    if data['conversation']['type'] == 'NEW':
+      self.start_conversation()
+      
+    else:
+      city = data['inputs'][0]['arguments'][0]['textValue'].lower()
+      self.get_weather(city)
+      
 def make_app():
   return tornado.web.Application([
     (r"/weather-app", WeatherHandler),
